8cc78936789e56f1125b3e2c5b25f9e8f28edfb2
MAX <psp.dsi.wii@gmail.com>
2016-08-31 20:07:50 +0900

    『ロボットプラネットユートピア』の追加

--- chunithm.js
+++ chunithm.js
@@ -324,6 +324,7 @@ var chart_list = [
     ,{ id: 308, level: 3, rate_base: 11.9, name: "fake!fake!" }
     ,{ id: 55,  level: 3, rate_base: 11.2, name: "夏祭り" }
     ,{ id: 66,  level: 3, rate_base: 12.3, name: "明るい未来" }
+    ,{ id: 261, level: 3, rate_base: 12.3, name: "ロボットプラネットユートピア" }
 ];
 
 // latest rate
