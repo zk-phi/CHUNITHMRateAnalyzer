c4894541c7f556d61aec7f9de26dd9395ac1dc73
max-eipi <psp.dsi.wii@gmail.com>
2016-10-25 01:22:12 +0900

    『Oshama Scramble! (Cranky Remix)』の追加

--- chunithm.js
+++ chunithm.js
@@ -394,6 +394,7 @@ var chart_list = [
     ,{ id: 300, level: 3, rate_base: 12.2, image: "img/012eb1ed09577836.jpg", name: "No Routine" }
     ,{ id: 190, level: 3, rate_base: 12.6, image: "img/bbaa464731ab96a4.jpg", name: "エテルニタス・ルドロジー" }
     ,{ id: 331, level: 3, rate_base: 12.2, image: "img/ec37e447b91995dd.jpg", name: "猛進ソリストライフ！" }
+    ,{ id: 259, level: 3, rate_base: 13.1, image: "img/4d66e5d1669d79a2.jpg", name: "Oshama Scramble! (Cranky Remix)" }
 ];
 
 // latest rate
