a7f583be21188d03bcb015134422f8c0858a5fed
max-eipi <psp.dsi.wii@gmail.com>
2016-11-18 02:01:48 +0900

    『ラクガキスト』の追加

--- chunithm.js
+++ chunithm.js
@@ -404,6 +404,7 @@ var chart_list = [
     ,{ id: 325, level: 3, rate_base: 12.6, image: "img/97eca622afca0f15.jpg", name: "Her Majesty" }
     ,{ id: 259, level: 2, rate_base: 11.3, image: "img/4d66e5d1669d79a2.jpg", name: "Oshama Scramble! (Cranky Remix) 赤" }
     ,{ id: 229, level: 2, rate_base: 11.9, image: "img/73f86aec8d6c7c9b.jpg", name: "紅華刑 赤" }
+    ,{ id: 281, level: 3, rate_base: 13.9, image: "img/330e57eeeb0fb2cd.jpg", name: "ラクガキスト" }
 ];
 
 // latest rate
