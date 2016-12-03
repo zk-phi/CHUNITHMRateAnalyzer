c4e12f43b67557561948159b1f82e1f7749c0758
max-eipi <psp.dsi.wii@gmail.com>
2016-12-04 00:19:58 +0900

    『ひだまりデイズ』の追加

--- chunithm.js
+++ chunithm.js
@@ -408,6 +408,7 @@ var chart_list = [
     ,{ id: 293, level: 3, rate_base: 13.2, image: "img/c58227eb0d14938c.jpg", name: "インビジブル" }
     ,{ id: 270, level: 3, rate_base: 12.3, image: "img/21dfcd3ae2c5c370.jpg", name: "エンヴィキャットウォーク" }
     ,{ id: 119, level: 3, rate_base: 12.3, image: "img/a7dd6716fcae0cb8.jpg", name: "アウターサイエンス" }
+    ,{ id: 313, level: 3, rate_base: 11.4, image: "img/5ac018495d6f01a5.jpg", name: "ひだまりデイズ" }
 ];
 
 // latest rate
